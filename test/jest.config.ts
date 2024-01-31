9aedc3e2468f6c09308b6e1265dfdbb877836f11
HideInDevelopment <hideindevelopment96@gmail.com>
2024-01-31 20:53:19 +0100

    modify testing every time git push is performed

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -3,4 +3,21 @@ module.exports = {
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/*spec.ts'],
+  testResultsProcessor: 'jest-sonar-reporter',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'reports',
+        outputName: 'jest-results.xml',
+      },
+    ],
+  ],
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text', 'text-summary', 'cobertura'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/test/'],
+  setupFiles: ['./test/setup.js'],
 };
