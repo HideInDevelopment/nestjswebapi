80d674397a370af76ac76fea84771d9afb387d09
HideInDevelopment <hideindevelopment96@gmail.com>
2024-02-01 00:35:28 +0100

    structure for user authentication. Swagger implementation. Code formatting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Main')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Entry endpoint',
+    description: `Don't do anything`,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Hello world',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,33 +9,75 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 // import { UserUtil } from 'src/utils/util.entities';
 
 @Controller('user')
+@ApiTags('User')
 export class UserController {
   constructor(private readonly usersService: UserService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all users',
+    description: 'Return a list of all users',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'A list of users',
+  })
   findAllUser(): Promise<User[]> {
     return this.usersService.findAllUsers();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get user by id',
+    description: 'Return a user by its id',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The user find by id',
+  })
   findUserById(@Param('id') id: number): Promise<User> {
     return this.usersService.findUserById(id);
   }
 
   @Get('email/:email')
+  @ApiOperation({
+    summary: 'Get user by email',
+    description: 'Return a user by its email',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The user find by email',
+  })
   findUserByEmail(@Param('email') email: string): Promise<User> {
     return this.usersService.findUserByEmail(email);
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Create a new user',
+    description: 'Creates a new user',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User created successfully',
+  })
   createUser(@Body() user: Partial<User>): Promise<User> {
     return this.usersService.createUser(user);
   }
 
   @Put(':id')
+  @ApiOperation({
+    summary: 'Update user by id',
+    description: 'Updates a user find by its id',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User updated successfully',
+  })
   updateUser(
     @Param('id') id: number,
     @Body() user: Partial<User>,
@@ -44,6 +86,14 @@ export class UserController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete user by id',
+    description: 'Delete a user find by its id',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User deleted successfully',
+  })
   deleteUser(@Param('id') id: number): Promise<void> {
     return this.usersService.removeUser(id);
   }
